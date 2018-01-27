39436aa38644bab9d9da35cf6e6d4c71303fda76
Kendall Wen <kendall.wen4@gmail.com>
2018-01-26 22:37:28 -0500

    Improved blank finding algorithm

--- minesweeper.py
+++ minesweeper.py
@@ -94,6 +94,55 @@ def full_board(board, rows, columns):
             add_numbers(board,i,j)
     return board
 
+# expand the board based on a player's move
+def expand(row, col, pboard, eboard):
+    pboard[row][col] = eboard[row][col]
+    if pboard[row][col]==' ':
+        visited = []
+        unvisited = []
+        unvisited.append([row,col])
+        # function to shorten typing to see if a spot has been visited
+        def found(vr,vc):
+            return [vr,vc] in visited or [vr,vc] in unvisited
+        while(len(unvisited)!=0):
+            cur = unvisited.pop()
+            r = cur[0]
+            c = cur[1]
+            visited.append(cur)
+            if r>0:
+                pboard[r-1][c] = eboard[r-1][c]
+                if eboard[r-1][c]==' 'and not found(r-1,c): 
+                    unvisited.append([r-1,c])
+                if c>0:
+                    pboard[r-1][c-1] = eboard[r-1][c-1]
+                    pboard[r][c-1] = eboard[r][c-1]
+                    if eboard[r][c-1]==' ' and not found(r,c-1):
+                        unvisited.append([r,c-1])
+                if c<len(pboard[r])-1:
+                    pboard[r-1][c+1] = eboard[r-1][c+1]
+                    pboard[r][c+1] = eboard[r][c+1]
+                    if eboard[r][c+1]==' ' and not found(r,c+1):
+                        unvisited.append([r,c+1])
+            if r<len(pboard)-1:
+                pboard[r+1][c] = eboard[r+1][c]
+                if eboard[r+1][c]==' ' and not found(r+1,c):
+                    unvisited.append([r+1,c])
+                if c>0:
+                    pboard[r+1][c-1] = eboard[r+1][c-1]
+                    if c>0:
+                        pboard[r+1][c-1] = eboard[r+1][c-1]
+                    if c<len(pboard[r])-1:
+                        pboard[r+1][c+1] = eboard[r+1][c+1]
+                            
+# count number of hidden spots on a board
+def count_hidden(board):
+    count = 0
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j]=='_': count = count+1
+    return count
+
+# main game flow
 def minesweeper():
     mines = int(input("How many mines would you like? ")) # Number of mines
     rows = int(input("How many rows in the board? ")) # Number of rows
@@ -101,132 +150,23 @@ def minesweeper():
     # this block creates the initial fully hidden board
     player_board = start_board(rows, cols)
     print_board(player_board)    
-    # user specified rows and columns
+    # mines added accordingly to player's first row and column choice
     frow = int(input("select row: "))
     fcolumn = int(input("select column: "))
     mine_board = add_mines(empty_board(rows, cols, frow, fcolumn), mines, rows, cols)
     end_board = full_board(mine_board,rows,cols)
-    
-    # count the number of blank spaces
-    def count_blank(blist,r,c):
-        if r < rows:
-            if c < cols:
-                if end_board[r][c] == ' ':
-                    blist.append([r,c])
-                    return count_blank(blist,r,c+1)
-                else: return count_blank(blist,r,c+1)
-            else: return count_blank(blist,r+1,0)
-        else: return blist
-    b_list = count_blank([],0,0)
-    
-    # determine how many blank spaces are above a spot on the board
-    def blank_above(bblock,pos):
-        if pos < len(b_list):
-            if (([b_list[pos][0]-1,b_list[pos][1]] in bblock or
-                 [b_list[pos][0]+1,b_list[pos][1]] in bblock or
-                 [b_list[pos][0],b_list[pos][1]+1] in bblock or
-                 [b_list[pos][0],b_list[pos][1]-1] in bblock)
-                and not [b_list[pos][0],b_list[pos][1]] in bblock):
-                bblock.append(b_list[pos])
-                return blank_above(bblock,pos+1)
-            else: return blank_above(bblock,pos+1)
-        else: return bblock
-        
-    # determine how many blank spaces are below a spot on the board
-    def blank_below(bblock,pos):
-        if pos >= 0:
-            if (([b_list[pos][0]-1,b_list[pos][1]] in bblock or
-                 [b_list[pos][0]+1,b_list[pos][1]] in bblock or
-                 [b_list[pos][0],b_list[pos][1]+1] in bblock or
-                 [b_list[pos][0],b_list[pos][1]-1] in bblock)
-                and not [b_list[pos][0],b_list[pos][1]] in bblock):
-                bblock.append(b_list[pos])
-                return blank_below(bblock,pos-1)
-            else: return blank_below(bblock,pos-1) 
-        else: return bblock
-        
-    # examine larger areas of blank space around a spot
-    def blank_block(r,c):
-        above = blank_above([[r,c]],0)
-        below = blank_below(above,len(b_list)-1)
-        return blank_above(below,0) 
-    
-    # determine the number of spots around a player's move to reveal
-    # if the move selects a blank spot
-    def freveal_list(rlist,r,c):    
-        blanks = blank_block(frow,fcolumn)
-        if r < rows:
-            if c < cols:
-                if ([r,c] in blanks or [r-1,c] in blanks or [r+1,c] in blanks 
-                    or [r,c-1] in blanks or [r,c+1] in blanks):
-                    rlist.append([r,c])
-                    return freveal_list(rlist,r,c+1)
-                else: return freveal_list(rlist,r,c+1)
-            else: return freveal_list(rlist,r+1,0)
-        else: return rlist
-        
-    # expand the players board such that they see the appropriate result
-    def fexpand_board(r,c):
-        if end_board[r][c] == 'm':
-            return print("GG please try again :(")
-        if end_board[r][c] == ' ':
-            reveal = freveal_list([],0,0)
-            def reveal_board(pos):
-                if pos<len(reveal):
-                    r1 = reveal[pos][0]
-                    c1 = reveal[pos][1]                    
-                    player_board[r1][c1] = end_board[r1][c1]
-                    return reveal_board(pos+1)
-                else: return
-            return reveal_board(0)
-        else:
-            player_board[r][c] = end_board[r][c]
-    fexpand_board(frow,fcolumn)
-    if str(player_board).count('_') == mines:
-        print("Congratulations! You win!")
-        print_board(end_board)
-    else:
-        print_board(player_board)
-        def nextturn():
-            nrow = int(input("select row: "))
-            ncolumn = int(input("select column: "))
-            if end_board[nrow][ncolumn] == 'm':
-                print("GG please try again :(")
-                print_board(end_board)
-                return
-            if end_board[nrow][ncolumn] == ' ':
-                def nreveal_list(rlist,r,c):    
-                    blanks = blank_block(nrow,ncolumn)
-                    if r < rows:
-                        if c < cols:
-                            if ([r,c] in blanks or [r-1,c] in blanks or 
-                                [r+1,c] in blanks or [r,c-1] in blanks or 
-                                [r,c+1] in blanks):
-                                rlist.append([r,c])
-                                return nreveal_list(rlist,r,c+1)
-                            else: return nreveal_list(rlist,r,c+1)
-                        else: return nreveal_list(rlist,r+1,0)
-                    else: return rlist
-                reveal = nreveal_list([],0,0)
-                def reveal_board(pos):
-                    if pos<len(reveal):
-                        r1 = reveal[pos][0]
-                        c1 = reveal[pos][1]
-                        player_board[r1][c1] = end_board[r1][c1]
-                        return reveal_board(pos+1)
-                reveal_board(0)
-                if str(player_board).count('_') == mines:
-                    print_board(end_board)
-                    return 'Congratulations! You win!'
-                else: 
-                    print_board(player_board)
-                    return nextturn()                
-            else:
-                player_board[nrow][ncolumn] = end_board[nrow][ncolumn]                
-                if str(player_board).count('_') == mines:
-                    print_board(end_board)
-                    return 'Congratulations! You win!'
-                else: 
-                    print_board(player_board)
-                    return nextturn()
-        return nextturn()
+    expand(frow,fcolumn,player_board,end_board)
+    print_board(player_board)
+    # game continues until the player wins or loses
+    while count_hidden(player_board)>mines:
+        row = int(input("select row: "))
+        col = int(input("select column: "))
+        if end_board[row][col]=='m':
+            print_board(end_board)
+            print("GG please try again")
+            return
+        else: 
+            expand(row,col,player_board,end_board)
+            print_board(player_board)
+    print("CONGRATULATIONS! YOU WIN")
+    return
